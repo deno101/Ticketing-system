4284e7a262019adddfbb00be9caf783c8c5f973d
Denniz <dennizwarui@gmail.com>
2018-10-26 07:59:58 +0300

    Change database from Sqlite3 to MySQL

--- Ticketing/settings.py
+++ Ticketing/settings.py
@@ -73,13 +73,21 @@ WSGI_APPLICATION = 'Ticketing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'ticketing',
+        'USER': 'SuperAdmin',
+        'PASSWORD': 'denniz',
+        'PORT': ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
